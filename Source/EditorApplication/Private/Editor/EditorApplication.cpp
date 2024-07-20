2fb4e9d7b448be128f3ae97e6b7cfb88a55c4e7f
Yassine Sz <0139ne@gmail.com>
2024-07-20 15:14:06 +0100

    remove the window test

--- Source/EditorApplication/Private/Editor/EditorApplication.cpp
+++ Source/EditorApplication/Private/Editor/EditorApplication.cpp
@@ -1,35 +1,11 @@
 #include <Editor/EditorApplication.hpp>
 
-#include <Engine/Engine.hpp>
-#include <Module/Core/FrameEventSubmodule.hpp>
-
-#include <Window/DesktopWindow.hpp>
-#include <Module/Graphics/RhiGraphicsSubmodule.hpp>
-
 namespace Ame
 {
     EditorApplication::EditorApplication(
         const EditorApplicationConfig& editorConfig) :
         Base(editorConfig.Application)
     {
-        auto& moduleRegistry = GetEngine().GetRegistry();
-
-        Ptr<FrameEventSubmodule> frameEvent;
-        moduleRegistry.GetModule(IID_CoreModule)->QueryInterface(IID_FrameEventSubmodule, frameEvent.DblPtr<IObject>());
-
-        Ptr<Window::IDesktopWindow> dekstopWindow;
-        moduleRegistry.GetModule(IID_GraphicsModule)->QueryInterface(Window::IID_DesktopWindow, dekstopWindow.DblPtr<IObject>());
-
-        frameEvent->OnFrameStart(
-            [this, dekstopWindow]()
-            {
-                dekstopWindow->ProcessEvents();
-
-                if (!dekstopWindow->IsRunning())
-                {
-                    GetEngine().Exit(0);
-                }
-            });
     }
 
     void EditorApplication::OnLoad()
